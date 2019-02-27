84a1b0f24c3a91e4ae68d0dec8a16d2f5f3cf13d
jennyjean <jennyjean8675309@yahoo.com>
2019-02-27 13:36:32 -0500

    initialized a store

--- src/index.js
+++ src/index.js
@@ -15,10 +15,8 @@ const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 ReactDOM.render(
   <Provider store={store}>
     <Router><App /></Router>
-  </Provider>, 
+  </Provider>,
       document.getElementById('root'));
-
-
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
