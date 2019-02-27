73e7abc2f1aa98bb19d53ce5f78a533ca7c4d63d
github username <github email address>
2019-02-27 15:00:08 -0500

    rails url added

--- src/Redux/actions.js
+++ src/Redux/actions.js
@@ -1,5 +1,7 @@
 const GENRES_URL = 'https://listennotes.p.rapidapi.com/api/v1/genres'
 
+const RAILS_URL = 'http://localhost:3000'
+
 const fetchGenres = (genres) =>{
   return {type: "GENRES", genres}
 }
