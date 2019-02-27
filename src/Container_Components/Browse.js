f83dc3d51de09f8eed6a3443e518fd0ba36957d4
jennyjean <jennyjean8675309@yahoo.com>
2019-02-27 13:33:09 -0500

    initialized a store and added redux-thunk

---
+++ src/Container_Components/Browse.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {fetchingGenres} from '../Redux/actions';
+import { connect } from 'react-redux';
+
+class Browse extends React.Component {
+  render() {
+    return (
+      <div>Browse page</div>
+    )
+  }
+}
+
+export default Browse;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import rootReducer from './Redux/Reducers/rootReducer';
+import thunk from 'redux-thunk';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router><App /></Router>
+  </Provider>, 
+      document.getElementById('root'));
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
