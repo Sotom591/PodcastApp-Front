bc51f92448e207cf83217e48f5cbf14528e5d089
jennyjean <jennyjean8675309@yahoo.com>
2019-03-15 11:30:05 -0400

    added fetch request for genres

--- src/Redux/Reducers/genresReducer.js
+++ src/Redux/Reducers/genresReducer.js
@@ -1,7 +1,11 @@
 const genresReducer = (state = [], action) =>{
   switch (action.type) {
-    case "GENRES":
-      return action.genres
+    case "GENRE":
+      let genres = [...state]
+      if (action.genre['parent_id'] === 67){
+        genres.push(action.genre)
+      }
+      return genres
     default:
       return state
   }
--- src/Redux/actions.js
+++ src/Redux/actions.js
@@ -3,19 +3,42 @@ const API_KEY = process.env.REACT_APP_LISTENNOTES_API_KEY
 
 const GENRES_URL = 'https://listennotes.p.rapidapi.com/api/v1/genres'
 
-const RAILS_URL = 'http://localhost:3000'
+const RAILS_URL = 'http://localhost:3000/api/v1'
 
-const fetchGenres = (genres) =>{
-  return {type: "GENRES", genres}
+const setGenre = (genre) =>{
+  return {type: "GENRE", genre}
+}
+
+//this action uses the results from the ListenNotes API fetch call to create Genres for our own backend API (localhost:3000)
+
+const creatingGenres = (item) =>{
+  let genre = {
+    "name": item.name,
+    "category_id": item.id,
+    "parent_id": item.parent_id
+  }
+  return (dispatch) =>{
+    fetch('http://localhost:3000/api/v1/genres', {
+      method: 'POST',
+      headers: {
+        "Content-type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(genre)
+    })
+    .then(response => response.json())
+    .then(data => {
+      dispatch(setGenre(data))
+    })
+  }
 }
 
 const fetchingGenres = () =>{
   return (dispatch) =>{
-    unirest.get("https://listennotes.p.rapidapi.com/api/v1/genres")
+    unirest.get(GENRES_URL)
     .header("X-RapidAPI-Key", API_KEY)
-    .end((result) => dispatch(fetchGenres(result.body.genres))
-  );
-  }
+    .end((result) => result.body.genres.forEach(genre => dispatch(creatingGenres(genre)))
+  )};
 }
 
 export {fetchingGenres}
